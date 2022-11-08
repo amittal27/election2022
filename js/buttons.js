18b7c5fa75b1ca7d610a80b45d0dae3fb8091bca
Angeli <angeli.mittal@gmail.com>
2022-11-08 16:53:01 -0600

    nonfunctional

--- js/buttons.js
+++ js/buttons.js
@@ -13,10 +13,15 @@ var obj = {
         console.log("board");
     },
     governor: function() {
-        
+        console.log("gubernatorial");
+        var map = document.getElementById("state-map");
+        map.innerHTML="";
+    
     },
     stateSenate: function() {
-
+        console.log("senate state");
+        var map = document.getElementById("state-map");
+        map.innerHTML="<div class=\"flourish-embed flourish-map\" data-src=\"visualisation/11750372\"><script src=\"https://public.flourish.studio/resources/embed.js\"></script></div>";
     },
     stateHouse: function() {
         
@@ -34,6 +39,6 @@ var obj = {
 
     },
     usHouse: function() {
-        
+
     }
 }
