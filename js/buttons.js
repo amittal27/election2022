f6fb6f789d2fda9cb57f227a60e2a03d6947ef4f
Angeli <angeli.mittal@gmail.com>
2022-11-08 21:02:28 -0600

    live site without maps

--- js/buttons.js
+++ js/buttons.js
@@ -6,39 +6,36 @@
 })
 
 var obj = {
-    clerk: function() {
-        console.log("clerk");
-    },
-    board: function() {
-        console.log("board");
-    },
     governor: function() {
-        console.log("gubernatorial");
         var map = document.getElementById("state-map");
-        map.innerHTML="";
-    
+        map.innerHTML = "<iframe src='https://flo.uri.sh/visualisation/11750879/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'></div>";
     },
     stateSenate: function() {
-        console.log("senate state");
-        var map = document.getElementById("state-map");
-        map.innerHTML="<div class=\"flourish-embed flourish-map\" data-src=\"visualisation/11750372\"><script src=\"https://public.flourish.studio/resources/embed.js\"></script></div>";
+        map = document.getElementById("state-map");
+        map.innerHTML = "<iframe src='https://flo.uri.sh/visualisation/11750372/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'></div>";
     },
     stateHouse: function() {
-        
+        map = document.getElementById("state-map");
+        map.innerHTML = "<iframe src='https://flo.uri.sh/visualisation/11750592/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'></div>";
     },
     attorney: function() {
-
+        map = document.getElementById("state-map");
+        map.innerHTML = "<iframe src='https://flo.uri.sh/visualisation/11750884/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'></div>";
     },
     secretary: function() {
-
+        map = document.getElementById("state-map");
+        map.innerHTML = "<iframe src='https://flo.uri.sh/visualisation/11750888/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'></div>";
     },
     referendum: function() {
-        
+        map = document.getElementById("state-map");
+        map.innerHTML = "<iframe src='https://flo.uri.sh/visualisation/11750622/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'></div>";
     },
     usSenate: function() {
-
+        map = document.getElementById("federal-map");
+        map.innerHTML = "<iframe src='https://flo.uri.sh/visualisation/11750744/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'></div>";
     },
     usHouse: function() {
-
+        map = document.getElementById("federal-map");
+        map.innerHTML = "<iframe src='https://flo.uri.sh/visualisation/11750757/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'></div>";
     }
 }
